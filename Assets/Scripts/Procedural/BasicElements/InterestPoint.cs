10f05ad51d07dd89c15dbb94060daf7c398ce89e
Javier Comas De Frutos <javiercomasdefrutos@gmail.com>
2024-03-04 12:47:45 +0100

    puntos de interes parte 1

---
+++ Assets/Scripts/Procedural/BasicElements/InterestPoint.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Procedural/InterestPoint")]
+
+[Serializable]
+public class InterestPoint : ScriptableObject
+{
+    /*COSAS QUE INTERNAMENTE NECESITARE
+        - Acceso a las Cells de cada chunk para saber si esa "casilla ya ha sido ocupada por otro objecto"
+        - USO DEL PATRON POISSON DISC para poder distribuir los puntos de interes siguiendo una serie de condiciones especificas como la distancia entre puntos
+        - 
+    */
+
+
+    /// <summary>
+    /// Type Of Intesrest Point
+    /// </summary>
+    public enum Type_of_POI
+    {
+        Resources,
+        Enemy,
+        Mission,
+        Allies,
+        Construction
+    };
+
+    [SerializeField]
+    public Type_of_POI typeOfPoint;
+
+    [SerializeField]
+    public GameObject objectGenerate;
+
+    [SerializeField]
+    public int amount;
+
+
+    [Header("Generate Settings")]
+    [SerializeField]
+    public float radius;
+
+    /// <summary>
+    /// Interpolación entre Puntos de Interés
+    /// </summary>
+    [SerializeField]
+    public bool interpolation;
+
+    void generatePoints(){
+       
+    }
+    
+}
